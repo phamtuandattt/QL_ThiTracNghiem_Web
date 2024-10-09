66a56c0504e38fb1a1df57a7eb25934b5097ee28
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-09 14:53:45 +0700

    resolve all file ignore

--- QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebAPI/obj/Debug/net8.0/QL_ThiTracNghiem_WebAPI.AssemblyInfo.cs
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebAPI/obj/Debug/net8.0/QL_ThiTracNghiem_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QL_ThiTracNghiem_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8073811096922ad52e560d8da6e4f7fc766ddf0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QL_ThiTracNghiem_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QL_ThiTracNghiem_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
