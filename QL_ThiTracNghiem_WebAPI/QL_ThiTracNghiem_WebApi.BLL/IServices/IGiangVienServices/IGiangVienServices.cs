1650b120682f225dd084e33f02264454037360d8
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-10 09:59:19 +0700

    Add GiangVienServices

---
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebApi.BLL/IServices/IGiangVienServices/IGiangVienServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_ThiTracNghiem_WebApi.BLL.IServices.IGiangVienServices
+{
+    public interface IGiangVienServices
+    {
+        IEnumerable<Giangvien> giangviens();
+        Giangvien giangvien(string magv);
+        void insert (Giangvien giangvien);
+        void delete (string magv);
+        void update(Giangvien giangvien);
+        void savechange();
+    }
+}
--- QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebApi.BLL/Services/GiangVienServices/GiangVienServices.cs
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebApi.BLL/Services/GiangVienServices/GiangVienServices.cs
@@ -1,9 +1,72 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+using QL_ThiTracNghiem_WebApi.BLL.IServices.IGiangVienServices;
 
 namespace QL_ThiTracNghiem_WebApi.BLL.Services.GiangVienServices
 {
-    public class GiangVienServices
+    public class GiangVienServices : IGiangVienServices
     {
+        private readonly QlHethongthitracnghiemContext _context;
 
+        public GiangVienServices(QlHethongthitracnghiemContext _context)
+        {
+            this._context = _context;
+        }
+
+        public void delete(string magv)
+        {
+            var gv = _context.Giangviens.Find(magv);
+            _context.Giangviens.Remove(gv);
+        }
+
+        public Giangvien giangvien(string magv)
+        {
+            return _context.Giangviens.Find(magv) ?? new Giangvien();
+        }
+
+        public IEnumerable<Giangvien> giangviens()
+        {
+            return _context.Giangviens.ToList();
+        }
+
+        public void insert(Giangvien giangvien)
+        {
+            _context.Add(giangvien);
+        }
+
+        public void savechange()
+        {
+            _context.SaveChanges();
+        }
+
+        public void update(Giangvien giangvien)
+        {
+            _context.Entry(giangvien).State = EntityState.Modified;
+        }
+
+        //private void UploadImage(IFormFile img, string webAlias, ref string fileNameInDb)
+        //{
+        //    try
+        //    {
+        //        if (!string.IsNullOrEmpty(webAlias))
+        //        {
+        //            var fileName = img.FileName;
+        //            fileName = fileName.Trim(Path.GetInvalidFileNameChars());
+
+        //            // Processing upload file to Azure
+        //            var azureStore = new AzureStore();
+        //            fileNameInDb = fileName;
+        //            // .net 6
+        //            var thumbnailStream = ImageHelper.GetThumbnailStream(img.OpenReadStream());
+        //            thumbnailStream.Position = 0;
+        //            azureStore.UploadImage(webAlias, thumbnailStream, ref fileNameInDb);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Logger.Error("ERROR: Upload file to azure CDN failed: ", ex);
+        //    }
+        //}
     }
 }
