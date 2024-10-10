ea7b34e228b08c4968127e2837725f9df6befe44
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-10 10:36:44 +0700

    Add ChuVuServices

---
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebApi.BLL/IServices/IChucVuServices/IChucVuServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_ThiTracNghiem_WebApi.BLL.IServices.IChucVuServices
+{
+    public interface IChucVuServices
+    {
+        IEnumerable<Chucvu> chucvus();
+        Chucvu chucvu(int macv);
+        void insert(Chucvu chucvu);
+        void delete(int mavc);
+        void update(Chucvu chucvu);
+        void savechange();
+    }
+}
---
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebApi.BLL/Services/ChucVuServices/ChucVuServices.cs
@@ -0,0 +1,49 @@
+﻿using QL_ThiTracNghiem_WebApi.BLL.IServices.IChucVuServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_ThiTracNghiem_WebApi.BLL.Services.ChucVuServices
+{
+    public class ChucVuServices : IChucVuServices
+    {
+        private readonly QlHethongthitracnghiemContext _context;
+
+        public ChucVuServices(QlHethongthitracnghiemContext context)
+        {
+            _context = context;
+        }
+
+        public Chucvu chucvu(int macv)
+        {
+            return _context.Chucvus.Find(macv) ?? new Chucvu();    
+        }
+
+        public IEnumerable<Chucvu> chucvus()
+        {
+            return _context.Chucvus.ToList() ?? new List<Chucvu>();
+        }
+
+        public void delete(int mavc)
+        {
+            _context.Chucvus.Remove(chucvu(mavc));
+        }
+
+        public void insert(Chucvu chucvu)
+        {
+            _context.Chucvus.Add(chucvu);
+        }
+
+        public void savechange()
+        {
+            _context.SaveChanges();
+        }
+
+        public void update(Chucvu chucvu)
+        {
+            _context.Entry(chucvu).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+    }
+}
