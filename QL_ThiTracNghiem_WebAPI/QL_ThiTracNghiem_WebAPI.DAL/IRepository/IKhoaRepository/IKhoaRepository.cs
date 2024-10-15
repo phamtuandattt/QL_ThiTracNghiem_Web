f3c46c4d476f7c4ba3679e382cab712ff7b1b703
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-15 10:17:46 +0700

    Add KhoRepository

---
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebAPI.DAL/IRepository/IKhoaRepository/IKhoaRepository.cs
@@ -0,0 +1,15 @@
+﻿using QL_ThiTracNghiem_WebAPI.DAL.Models;
+
+
+namespace QL_ThiTracNghiem_WebAPI.DAL.IRepository.IKhoaRepository
+{
+    public interface IKhoaRepository
+    {
+        Task<Khoa> GetByIdAsync(string makhoa);
+        Task<List<Khoa>> GetAllAsync();
+        Task AddAsync(Khoa khoa);
+        Task UpdateAsync(Khoa khoa);
+        Task DeleteAsync(string makhoa);
+        Task<bool> ExistsAsync(string makhoa);
+    }
+}
---
+++ QL_ThiTracNghiem_WebAPI/QL_ThiTracNghiem_WebAPI.DAL/Repository/KhoaRepository/KhoaRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QL_ThiTracNghiem_WebAPI.DAL.IRepository.IKhoaRepository;
+using QL_ThiTracNghiem_WebAPI.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_ThiTracNghiem_WebAPI.DAL.Repository.KhoaRepository
+{
+    public class KhoaRepository : IKhoaRepository
+    {
+        private readonly QlHethongthitracnghiemContext _context;
+        public KhoaRepository(QlHethongthitracnghiemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Khoa khoa)
+        {
+            await _context.AddAsync(khoa);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string makhoa)
+        {
+            var item = await _context.Khoas.FindAsync(makhoa);
+            if (item != null)
+            {
+                _context.Khoas.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> ExistsAsync(string makhoa)
+        {
+            return await _context.Khoas.AnyAsync(m => m.Makhoa == makhoa);
+        }
+
+        public async Task<List<Khoa>> GetAllAsync()
+        {
+            return await _context.Khoas.ToListAsync();
+        }
+
+        public async Task<Khoa> GetByIdAsync(string makhoa)
+        {
+            return await _context.Khoas.FindAsync(makhoa) ?? new Khoa();
+        }
+
+        public async Task UpdateAsync(Khoa khoa)
+        {
+            _context.Khoas.Update(khoa);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
