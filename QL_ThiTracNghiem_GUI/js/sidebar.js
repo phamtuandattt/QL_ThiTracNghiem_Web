21ea7531ea0ee450dbe61f3926e9b89ce29dd66c
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-24 14:40:21 +0700

    Update _Layout

--- QL_ThiTracNghiem_GUI/js/sidebar.js
+++ QL_ThiTracNghiem_GUI/js/sidebar.js
@@ -4,3 +4,17 @@ document.querySelectorAll('.sidebar-item').forEach(item => {
         this.classList.add('active');
     });
 });
+
+// JavaScript to toggle the dropdown visibility
+document.querySelector('.profile-button').addEventListener('click', function() {
+    const dropdown = document.querySelector('.profile-dropdown');
+    dropdown.classList.toggle('open');
+});
+
+//Close the dropdown if you click outside of it
+document.addEventListener('click', function(e) {
+    const profileDropdown = document.querySelector('.profile-dropdown');
+    if (!profileDropdown.contains(e.target)) {
+        profileDropdown.classList.remove('open');
+    }
+});
