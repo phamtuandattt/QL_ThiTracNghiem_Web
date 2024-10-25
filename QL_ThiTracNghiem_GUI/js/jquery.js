01a75f455f45d9855b5b4d3c8795513c21bed02b
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-25 10:17:05 +0700

    update sidebar and header

--- QL_ThiTracNghiem_GUI/js/jquery.js
+++ QL_ThiTracNghiem_GUI/js/jquery.js
@@ -5,11 +5,16 @@ function toggleSidebar() {
 
     var btntogle = document.querySelector('.toggle-btn');
     btntogle.classList.toggle('expanded');
-    
+
+    const element = document.querySelector("body > div > div.content")
     var toggleBtn = document.querySelector('.toggle-btn span');
     if (sidebar.classList.contains('expanded')) {
-        toggleBtn.innerHTML = '&#10094;'; // Collapse icon
+        // toggleBtn.innerHTML = '&#10094;'; // Collapse icon
+        element.style.margin = '0 0 0 270px';
+        element.style.transition = 'margin 0.3s ease'   
     } else {
-        toggleBtn.innerHTML = '&#10095;'; // Expand icon
+        // toggleBtn.innerHTML = '&#10095;'; // Expand icon
+        element.style.margin = '0 0 0 80px';  
+        element.style.transition = 'margin 0.3s ease'     
     }
 }
