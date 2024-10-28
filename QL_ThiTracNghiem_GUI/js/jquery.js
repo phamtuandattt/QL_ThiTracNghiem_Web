ce35765db8433736a9fa9fd4873769d49b1fc779
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-10-28 15:41:06 +0700

    Update popup

--- QL_ThiTracNghiem_GUI/js/jquery.js
+++ QL_ThiTracNghiem_GUI/js/jquery.js
@@ -11,23 +11,54 @@ function toggleSidebar() {
     if (sidebar.classList.contains('expanded')) {
         // toggleBtn.innerHTML = '&#10094;'; // Collapse icon
         element.style.margin = '0 0 0 270px';
-        element.style.transition = 'margin 0.3s ease'   
+        element.style.transition = 'margin 0.3s ease'
     } else {
         // toggleBtn.innerHTML = '&#10095;'; // Expand icon
-        element.style.margin = '0 0 0 80px';  
-        element.style.transition = 'margin 0.3s ease'     
+        element.style.margin = '0 0 0 80px';
+        element.style.transition = 'margin 0.3s ease'
     }
 }
 
 function showPopup(name, age) {
     const popup = document.getElementById('popup');
-    document.getElementById('popup-title').innerText = name;
-    document.getElementById('popup-info').innerText = 'Age: ' + age;
-    
+    document.getElementById('popup-title').innerText = 'Nội dung câu hỏi';
+
     popup.classList.add('show'); // Add the 'show' class to display the popup
 }
 // Function to close the popup with a fade-out effect
 function closePopup() {
     const popup = document.getElementById('popup');
-    popup.classList.remove('show'); // Start the fade-out transition
+
+    const radioButtons = document.querySelectorAll('#popup-answer input[type="radio"]');
+    radioButtons.forEach(radio => {
+        radio.disabled = true;
+    });
+    
+    popup.classList.remove('show'); // Start the fade-out transition  
 }
+function enableEditing() {
+    // Get all radio buttons in the form
+    const radioButtons = document.querySelectorAll('#popup-answer input[type="radio"]');
+    // Enable each radio button
+    radioButtons.forEach(radio => {
+        radio.disabled = false;
+    });
+}
+function disableEditing() {
+    const radioButtons = document.querySelectorAll('#popup-answer input[type="radio"]');
+    radioButtons.forEach(radio => {
+        radio.disabled = true;
+    });
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.getElementById("img-question");
+    const container_img_question = document.getElementById("image-section");
+    // Check if `src` attribute has a value
+    if (image.src == "" || image.src.endsWith("/")) {
+        // If `src` is empty or invalid, hide the image
+        container_img_question.style.display = "none";
+    } else {
+        // Show the image if `src` has a valid value
+        container_img_question.style.display = "block";
+    }
+});
