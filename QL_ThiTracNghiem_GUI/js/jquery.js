6eed0d33f1607a5f68c6d2720192ba879ba359cf
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2024-11-14 07:56:44 +0700

    Update and Create TaoCaThi.html v1

--- QL_ThiTracNghiem_GUI/js/jquery.js
+++ QL_ThiTracNghiem_GUI/js/jquery.js
@@ -9,18 +9,21 @@ function toggleSidebar() {
     const element = document.querySelector("body > div > div.content")
     var toggleBtn = document.querySelector('.toggle-btn span');
 
-    const submenu = document.getElementById('dethi-menu');
+    const submenuDeThi = document.getElementById('dethi-menu');
+    const submenuCaThi = document.getElementById('taocathi-menu');
     
     if (sidebar.classList.contains('expanded')) {
         // toggleBtn.innerHTML = '&#10094;'; // Collapse icon
         element.style.margin = '0 0 0 270px';
         element.style.transition = 'margin 0.3s ease'
-        submenu.classList.add('expanded');
+        submenuDeThi.classList.add('expanded');
+        submenuCaThi.classList.add('expanded');
     } else {
         // toggleBtn.innerHTML = '&#10095;'; // Expand icon
         element.style.margin = '0 0 0 80px';
         element.style.transition = 'margin 0.3s ease'
-        submenu.classList.remove('expanded');
+        submenuDeThi.classList.remove('expanded');
+        submenuCaThi.classList.remove('expanded');
     }
 }
 
